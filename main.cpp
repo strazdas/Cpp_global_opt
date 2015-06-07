17aa7ccef7c5969363b9edef6036a071eab5348f
niekas <albertas.gimbutas@mii.vu.lt>
2015-06-07 15:37:42 +0300

    Add experiment statistics printing implementation.

--- main.cpp
+++ main.cpp
@@ -4,29 +4,39 @@
 using namespace std;
 
 
+void print_stats(int calls[], int subregions[], int n) {
+    sort(calls, calls+n);
+    sort(subregions, subregions+n);
+    int calls_sum = 0;
+    for (int i=0; i < n; i++) {
+        calls_sum += calls[i];
+    };
+    cout << "Calls50: " << calls[49] << " Calls100: " << calls[99] << " Average: " << calls_sum/100. <<
+        " Subregions50: " << subregions[49] << " Subregions100: " << subregions[99] << endl; 
+};
+
 int main() {
     GKLSFunction* func;
     Disimplv* alg;
-    for (int cls=1; cls <= 1; cls++) {
+    int n = 100;
+    for (int cls=5; cls <= 5; cls++) {
         int calls[100];
-        for (int fid=1; fid <= 100; fid++) {
-            fid = 3;
+        int subregions[100];
+        for (int fid=1; fid <= n; fid++) {
+    //         // fid = 94;
             alg = new Disimplv(1.0, 30000);
             func = new GKLSFunction(cls, fid);    // Class1, id80
             alg->minimize(func);
             cout << fid << ". ";
             func->print();
             calls[fid-1] = func->_calls;
+            subregions[fid-1] = alg->_partition.size();
             delete alg;
             delete func;
             break;
         };
-        int calls_sum = 0;
-        for (int i=0; i< 100; i++) {
-            calls_sum += calls[i];
-        };
-        // cout << "Class " << cls << " average: " << calls_sum/100. << endl; 
-        break;
+    //     // cout << "Class " << cls << " "; 
+    //     // print_stats(calls, subregions, n);
     };
     GKLS_free();
     GKLS_domain_free();
