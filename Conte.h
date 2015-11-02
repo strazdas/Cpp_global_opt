5d8640d03f34fde64e87af1c342511274114d396
niekas <albertas.gimbutas@mii.vu.lt>
2015-11-02 18:40:09 +0200

    Optimize Conte algorithm

--- Conte.h
+++ Conte.h
@@ -25,152 +25,70 @@ class Conte {   // For efficiency do not store links to parents
 public:
     Conte(vector<Point*> verts, double L) {
         _verts = verts;
-        sort(_verts.begin(), _verts.end(), Point::compare_by_value);     // When several longest edges exist: randomness? // Note: Should sorting be done descending?
         _L = L;
         _D = _verts.size() - 1;
-        _partition.push_back(new Subsimplex(_verts, _L));
-        _accuracy = 1e-1;    // should be proportional to problem dimension
-        _max_iter = 10;      // This means max number of function evalutaions
+        _V = _verts.size();
     };
-    vector<Point*> _verts;   // Simplex vertexes used for initialization
+    vector<Point*> _verts;   // Simplex vertexes
     double _L;
     int _D;
-    vector<Subsimplex*> _partition;
-    vector<Point*> _points;  // Cashe of Point objects to be deleted
-    double _accuracy;        // Stopping criteria (tolerance threshold)
-    int _max_iter;           // This means max number of function evalutaions
-    
-    double get_lb_value(vector<Point*> verts, double L, Point* point) {
+    int _V;
+
+    double get_lb_value(double* p) {
         /* Returns lb value at a given point */
-        double lb_value = -numeric_limits<double>::max();  // Note check numeric_limits<double>min() usage, because the value is 2e-308 ~ 0, and not negative.
+        double cone_value;
         double dist;
-        int min_i = numeric_limits<int>::min();
-        // 2e-308 value is calculated here, is it correct?
-        for (int i=0; i < verts.size(); i++) {
-            dist = l2norm(point, verts[i]);
-            // cout << lb_value << " is greater thant " << verts[i]->_values[0] - L*dist << endl;
-            if (lb_value < verts[i]->_values[0] - L*dist) {
-                lb_value = verts[i]->_values[0] - L*dist;
-                min_i = i;
+        double lb_value = -numeric_limits<double>::max();;
+
+        for (int i=0; i < _verts.size(); i++) {
+            // dist = l2norm(point, verts[i]);
+            dist = 0;
+            for (int j=0; j < _verts[i]->size(); j++){
+                dist += pow(_verts[i]->_X[j] - p[j], 2);
+            };
+            dist = sqrt(dist);
+
+            cone_value = _verts[i]->_values[0] - _L*dist;
+            if (lb_value < cone_value) {
+                lb_value = cone_value;
             };
         };
         return lb_value;
     };
 
-    vector<Subsimplex*> divide_subsimplex(Subsimplex* subsimplex, string strategy="longest_half") {
-        vector<Subsimplex*> divided_subsimplexes;
+    Point* minimize() {       // Returns estimated simplex_lb_min_value
+        double best_value;
+        double best_p[_D];
+        double p[_D];
+        double coef;
+        double value;
 
-        if (strategy== "longest_half") {
-            // Find middle point
-            double c[_D];
-            for (int i=0; i < _D; i++) {
-                c[i] = (subsimplex->_le_v1->_X[i] + subsimplex->_le_v2->_X[i]) / 2.;
-            };
-
-            Point* middle_point = new Point(c, _D);
-
-            _points.push_back(middle_point);
-            middle_point->add_value(get_lb_value(_verts, _L, middle_point));
-
-            // Construct two new simplexes using this middle point.
-            Subsimplex* left_subsimplex = new Subsimplex(subsimplex->_L);
-            Subsimplex* right_subsimplex = new Subsimplex(subsimplex->_L);
-
-            for (int i=0; i < _D + 1; i++){
-                // Point* point = _func->get(new Point(triangle[i], n)); 
-                if (subsimplex->_verts[i] != subsimplex->_le_v1){
-                    right_subsimplex->add_vertex(subsimplex->_verts[i]);
-                } else {
-                    right_subsimplex->add_vertex(middle_point);
-                };
-                if (subsimplex->_verts[i] != subsimplex->_le_v2) {
-                    left_subsimplex->add_vertex(subsimplex->_verts[i]);
-                } else {
-                    left_subsimplex->add_vertex(middle_point);
+        for (int i=0; i < CS_LEN[_D-2]; i++) {   // Iterates thgrough coeficients
+            for (int j=0; j < _V; j++) { p[j] = 0; };
+            for (int k=0; k < _V; k++) {  // Iterates through coeficient components
+                coef = CS[_D-2][_V*i + k];
+                // Iterates through point components
+                for (int j=0; j < _D; j++) {
+                    p[j] += coef * _verts[k]->_X[j];
                 };
             };
-            left_subsimplex->init_parameters();
-            right_subsimplex->init_parameters();
+            value = get_lb_value(p);
 
-            divided_subsimplexes.push_back(left_subsimplex);
-            divided_subsimplexes.push_back(right_subsimplex);
-        };
-        return divided_subsimplexes;
-    };
-
-    // int get_simplex_to_divide_id(vector<Subsimplex*> subsimplexes) {
-    //     // Selects subsimplex with lowest estimated lower bound minimum
-    //     int simplex_with_min_lb_value_id = 0;
-    //     for (int i=0; i < subsimplexes.size(); i++) {
-    //         if (subsimplexes[simplex_with_min_lb_value_id]->_min_lb_value < subsimplexes[i]->_min_lb_value) {
-    //             simplex_with_min_lb_value_id = i;
-    //         };
-    //     };
-    //     return simplex_with_min_lb_value_id;
-    // };
-
-    vector<Subsimplex*> divide_subsimplexes(vector<Subsimplex*> simpls) {
-        vector<Subsimplex*> divided_subsimplexes;
-        for (int i=0; i < simpls.size(); i++) {
-            vector<Subsimplex*> two_subsimplexes = divide_subsimplex(simpls[i]);
-            for (int j=0; j < two_subsimplexes.size(); j++) {
-                divided_subsimplexes.push_back(two_subsimplexes[j]);
+            if (value < best_value) {
+                best_value = value;
+                for (int j=0; j < _V; j++) {
+                    best_p[j] = p[j];
+                };
             };
         };
-        return divided_subsimplexes;
-    };
 
-
-    Point* minimize() {     // Returns estimated simplex_lb_min_value
-        double tolerance = _partition[0]->_tolerance;
-
-
-        int iter = 0;
-        // while (tolerance > _accuracy) {
-        while (_max_iter > iter) {
-            // Pop subsimplex whith lowest min lb value from partition
-            // Subsimplex* to_divide = _partition[0];
-            // _partition.erase(_partition.begin() + 0);
-
-            // Divide selected subsimplexes
-            // vector<Subsimplex*> divided_subsimplexes = divide_subsimplex(to_divide);
-            vector<Subsimplex*> divided_subsimplexes = divide_subsimplexes(_partition);
-
-            // Remove all _partition subsimplexes.    Note: Does this work?
-            for (int i=0; i < _partition.size(); i++) {
-                delete _partition[i];
-            };
-            _partition.clear();
-
-            // Erase to_divide
-            // delete to_divide;
-
-            // insert new simplexes as bubble sort
-            // Iterate through
-
-            for (int i=0; i < divided_subsimplexes.size(); i++) { // Note: structure which has instert 
-                _partition.push_back(divided_subsimplexes[i]);    // command would allow to insert as bubble sort  
-            };
-
-            // sort partition by min lb value 
-            sort(_partition.begin(), _partition.end(), Subsimplex::compare_by_min_lb_value);
-
-            tolerance = _partition[0]->_tolerance;
-            iter += 1;
-            // cout << it << ". tol " << tolerance << " acc " << _accuracy << " estimate " << _partition[0]->_min_vert_value << " min_lb " << _partition[0]->_min_lb_value << " diameter " << _partition[0]->_diameter << endl;
-        };
-        return _partition[0]->_min_vert;
+        // Construct Point object
+        Point* lb_estimate = new Point(best_p, _V);
+        lb_estimate->add_value(best_value);
+        return lb_estimate;
     };
 
     virtual ~Conte() {
-        for (int i=0; i < _points.size(); i++) {
-            delete _points[i];
-        };
-        _points.clear();
-        for (int i=0; i < _partition.size(); i++) {
-            delete _partition[i];
-        };
-        _partition.clear();
     };
 };
 
